02722be8644892e279590edc801f53069f044ddb
Abhimanyu <abhibatule@gmail.com>
2023-01-30 16:44:11 +0530

    it's written first time

---
+++ API/test.py
@@ -0,0 +1,40 @@
+import flask
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/abc', methods=['GET', 'POST'])
+def test():
+    if (request.method=='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a +b
+        return jsonify(str(result))
+
+@app.route('/abc/mul', methods=['GET', 'POST'])
+def test1():
+    if (request.method=='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a * b
+        return jsonify(str(result))
+
+@app.route('/abc/div', methods=['GET', 'POST'])
+def test2():
+    if (request.method=='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a / b
+        return jsonify(str(result))
+
+@app.route('/abc/sub', methods=['GET', 'POST'])
+def test3():
+    if (request.method=='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a - b
+        return jsonify(str(result))
+
+if __name__=="__main__":
+    app.run()
